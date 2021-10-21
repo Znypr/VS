0e012ec4825daf8451598e96e32f82cb84a85ae3
Znypr <mulyadhic@gmail.com>
2021-10-22 00:41:37 +0200

    Update SyslogServer.java
    bufsize update, decoding

--- src/LoggingService/SyslogServer.java
+++ src/LoggingService/SyslogServer.java
@@ -3,6 +3,11 @@ package LoggingService;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SyslogServer {
@@ -10,17 +15,18 @@ public class SyslogServer {
 
 	private static final int PORT = 4712; 
 
-	private static final int BUFSIZE = 512;
+	private static final int BUFSIZE = 2048; //SHOULD msg length
 
 
 	public static void main(String[] args) {
+		
+
+		//CharBuffer msg = StandardCharsets.UTF_8.decode(ByteBuffer);
 		
 		try (DatagramSocket socket = new DatagramSocket(PORT)) {
 			
 			DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
 			// kein packetOut notwendig, da keine Antwort erwartet wird
-
-			System.out.println("Server gestartet ...");
 
 			while (true) {
 				socket.receive(packetIn);
