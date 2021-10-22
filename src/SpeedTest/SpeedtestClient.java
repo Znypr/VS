5d6419b272e924d3159e7051a8bad26c3684f63a
Znypr <mulyadhic@gmail.com>
2021-10-22 22:16:29 +0200

    speedtest first concept

--- src/SpeedTest/SpeedtestClient.java
+++ src/SpeedTest/SpeedtestClient.java
@@ -1,9 +1,11 @@
 package SpeedTest;
 
+import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
 
 public class SpeedtestClient {
 	
@@ -12,22 +14,40 @@ public class SpeedtestClient {
 
 	  private static final int PORT = 4712;
 
-	  private static final int BUFSIZE = 512;
+	  private static final String BUFSIZE = "256";
+	  
+	  private static int byteLength = 3;
 
-	  private static final int TIMEOUT = 2000;
+	  private static final int TIMEOUT = 200000;
+	  
+	  
+	  private static int getBufSize() {
+		  return Integer.valueOf(BUFSIZE);
+	  }
 
 	  
 	public static void main(String[] args) {
 		
 	    try (DatagramSocket socket = new DatagramSocket()) {
+	    	
 	        socket.setSoTimeout(TIMEOUT); 
-	        InetAddress iaddr = InetAddress.getByName(HOST);
-	        DatagramPacket packetOut = new DatagramPacket(new byte[0], 0, iaddr, PORT);
-	        socket.send(packetOut);
-	        DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
+	        
+	        // send test request #1
+	        InetAddress iaddr = InetAddress.getByName(HOST);	        	        
+	        byte[] bytes = BUFSIZE.getBytes();	       
+	        DatagramPacket testRequest = new DatagramPacket(bytes, byteLength, iaddr, PORT);
+	        socket.send(testRequest);
+	        
+	        // receive test acknowledgement #4
+	        DatagramPacket packetIn = new DatagramPacket(new byte[byteLength], byteLength);
 	        socket.receive(packetIn);
-	        String received = new String(packetIn.getData(), 0, packetIn.getLength());
-	        System.out.println("Received: " + received);
+	        System.out.println("received");
+
+	        // send data #5
+	        DatagramPacket packetOut = new DatagramPacket(new byte[getBufSize()], getBufSize(), iaddr, PORT);
+	        socket.send(packetOut);
+
+	       
 	      } catch (SocketTimeoutException e) {
 	        System.err.println("Timeout: " + e.getMessage());
 	      } catch (Exception e) {
--- src/SpeedTest/SpeedtestServer.java
+++ src/SpeedTest/SpeedtestServer.java
@@ -1,35 +1,82 @@
 package SpeedTest;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 import java.util.Date;
+
 
 public class SpeedtestServer {
 
 
 	private static final int PORT = 4712;
 
-	private static final int BUFSIZE = 512;
+	private static final int BUFSIZE = 256;
+	
+	private static final int MAXBUF = 3;
+	
+	private static int BUF = MAXBUF;
 
 
 	public static void main(String[] args) {
 
 		try (DatagramSocket socket = new DatagramSocket(PORT)) {
-			DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
-			DatagramPacket packetOut = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
-
-			System.out.println("Server gestartet ...");
-
+			
+			System.out.println("starting server..\n");		
+			long startingTime = 0;
+			
+			
 			while (true) {
+				
+				DatagramPacket packetIn = new DatagramPacket(new byte[BUF], BUF);
+				
 				socket.receive(packetIn);
-				System.out.println(
-						"Received from: " + packetIn.getAddress().getHostAddress() + ":" + packetIn.getPort());
-				String jetzt = (new Date()).toString();
-				packetOut.setData(jetzt.getBytes());
-				packetOut.setLength(jetzt.length());
-				packetOut.setSocketAddress(packetIn.getSocketAddress());
-				socket.send(packetOut);
+				System.out.println("message received: l-" + packetIn.getLength());	
+				long timestamp = System.nanoTime();
+				int dataSize = 0;
+				
+				if (BUF == MAXBUF) {
+					
+					byte[] bytes = packetIn.getData();
+					String data = new String(packetIn.getData());
+					dataSize = Integer.valueOf(data);
+				}
+				
+				// receive test request #2
+				if (packetIn.getData().length  <= MAXBUF) {
+					
+					// send test acknowledgement #3
+					if (dataSize <= BUFSIZE) {
+						DatagramPacket packetOut = new DatagramPacket(new byte[BUF], BUF);
+						packetOut.setSocketAddress(packetIn.getSocketAddress());
+						packetOut.setData(packetIn.getData());
+						socket.send(packetOut);
+						System.out.println("achknowledgement sent");	
+						BUF = dataSize;
+						startingTime = System.nanoTime();
+					} else {
+						System.out.println("Request denied");
+					}
+				} 
+				// receive data #6
+				else {
+					double delta = (timestamp - startingTime) / 1e6;
+					System.out.println("Results: ");
+					System.out.println("  " + packetIn.getLength() + " bytes received in " + delta + "ms.");
+					DecimalFormat f = new DecimalFormat("##.00");
+					String speed = f.format(BUF/delta*8);
+					System.out.println("  Speed: " + speed + " MB/s");
+					BUF = MAXBUF;
+				}
+				
+				
+
 			}
 		} catch (final IOException e) {
 			System.err.println(e);
