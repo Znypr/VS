c14e6c324b09a386ec6eae5cfad209c407f028bf
Znypr <mulyadhic@gmail.com>
2021-10-22 16:46:06 +0200

    Update SyslogClient.java
    Server

--- src/LoggingService/SyslogClient.java
+++ src/LoggingService/SyslogClient.java
@@ -1,14 +1,20 @@
 package LoggingService;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class SyslogClient {
 
@@ -20,9 +26,42 @@ public class SyslogClient {
 
 	private static final int PORT = 4712;
 
-	private static final int BUFSIZE = 512;
+	private static final int BUFSIZE = 2048;
 
 	private static final int TIMEOUT = 2000;
+	
+	private static String BOM = "BOM";
+	
+	
+	private static String getFQDN() {
+		
+		String domainname = "";
+		
+		try {
+			domainname = InetAddress.getLocalHost().getCanonicalHostName();
+		} catch (Exception e) {
+			System.err.println("Timeout: " + e.getMessage());
+		}
+		return domainname;
+	}
+	
+    private static InetAddress getBroadcast() {
+        
+        NetworkInterface nif =null;
+        try {
+            nif = NetworkInterface.getByIndex(1);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        List<java.net.InterfaceAddress> list = nif.getInterfaceAddresses();          
+        try {
+            return InetAddress.getByName(list.get(0).getBroadcast().toString().replaceFirst("/", ""));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
 	
 	private static String getPID() {
 		return ManagementFactory.getRuntimeMXBean().getName();
@@ -32,27 +71,72 @@ public class SyslogClient {
 		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(new Date());
 	}
 
-	private static String getPRI(int facility, int severity) {
-		return "<" + facility*8+severity + ">";
+	private static String getPRI(String facility, String severity) {
+
+		int fV = Integer.valueOf(facility);
+		int sV = Integer.valueOf(severity);
+		
+		return "<" + fV*8+sV + ">";
 	}
 
+	private static String createHeader(String facility, String severity, String msgID) {
+		
+		String pri = getPRI(facility, severity);
 
+		String timestamp = getTimeStamp();
+		String pid = getPID();
+
+		
+		return pri + version + " " + timestamp + " " + getFQDN() + " " + appName + " " + pid + " " + msgID + " ";
+	}
+	
+	private static String buildMsg(String msg) {
+		return " " + BOM + msg;
+	}
+	
+	private static byte[] buildSyslogMsg(String header, String sd, String msg) {
+		
+		ByteArrayOutputStream con = new ByteArrayOutputStream();
+		
+		byte[] byteheader = header.getBytes(StandardCharsets.US_ASCII);
+		byte[] byteSD = sd.getBytes();
+		byte[] byteMsg = msg.getBytes(StandardCharsets.UTF_8);
+		
+		try {
+			con.write(byteheader);
+			con.write(byteSD);
+			con.write(byteMsg);
+		} catch (Exception e) {
+			System.err.println("Timeout: " + e.getMessage());
+		}
+		
+		return con.toByteArray();
+	}
+	
 	public static void main(String[] args) {
 		
-		ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode("");
-
-
 		try (DatagramSocket socket = new DatagramSocket()) {
-			String pri = getPRI(0,0);
-			String hostname = InetAddress.getLocalHost().getHostName();
-			String timestamp = getTimeStamp();
-			String pid = getPID();
-			String msgID = ""; 
+						
+			// HEADER
+			String header = createHeader(args[0], args[1], args[2]);
 			
-			socket.setSoTimeout(TIMEOUT); 
-			InetAddress iaddr = InetAddress.getByName(HOST);
-			DatagramPacket packetOut = new DatagramPacket(new byte[0], 0, iaddr, PORT);
-			socket.send(packetOut);
+			// STRUCTURED DATA
+			String sd = "-";
+			
+			// MSG
+			String msg = buildMsg(args[3]);
+			
+			// Syslog Formatted Message
+			byte[] syslogMsg = buildSyslogMsg(header, sd, msg);
+			
+			
+			// AUTO DISCOVERY
+			InetAddress iaddr = getBroadcast();
+			
+			DatagramPacket packetOut = new DatagramPacket(syslogMsg, syslogMsg.length, iaddr, PORT);
+			if (syslogMsg.length <= BUFSIZE) {				
+				socket.send(packetOut);
+			}
 			
 		} catch (SocketTimeoutException e) {
 			System.err.println("Timeout: " + e.getMessage());
