ff93b75427486f724e05a60af91e5b31a98a87da
Mike <mike.eirich@gmx.de>
2021-10-24 15:22:49 +0200

    gemeinsame lösung stand jetzt

--- src/LoggingService/SyslogClient.java
+++ src/LoggingService/SyslogClient.java
@@ -1,31 +1,178 @@
 package LoggingService;
 
+import java.io.ByteArrayOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SyslogClient {
 
+	private static int VERSION = 1;
 
-	private static final String HOST = "localhost";
+//	private static final String HOST = getFQDN();
 
-	private static final int PORT = 4712;
+	private static final String BOM = "BOM";
 
-	private static final int BUFSIZE = 512;
+//	private static final String APPNAME = SyslogClient.class.getName();
+
+	private static final int PORT = 4711;
+
+	private static final int BUFSIZE = 2048;
 
 	private static final int TIMEOUT = 2000;
 
+	private static String getPID() {
+		return Long.toString(ProcessHandle.current().pid());
+	}
+
+	private static String getTimeStamp() {
+		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(new Date());
+	}
+
+	private static String getPRI(String facility, String severity) {
+
+		int faciliyCode = getFacilityCode(facility);
+		int severityCode = getSeverityCode(severity);
+		int priorityValue = faciliyCode * 8 + severityCode;
+
+		return "<" + priorityValue + ">";
+	}
+
+//	private static String createHeader(String facility, String severity, String msgID) {
+//
+//		String pri = getPRI(facility, severity);
+//		String timestamp = getTimeStamp();
+//		String pid = getPID();
+//
+//		return pri + VERSION + " " + timestamp + " " + HOST + " " + APPNAME + " " + pid + " " + msgID + " ";
+//	}
+	
+	private static String createHeader(String PRI, String TIMESTAMP, String HOSTNAME, String APPNAME, String PROCID, String MSGID) {
+		return PRI + VERSION + " " + TIMESTAMP + " " + HOSTNAME + " " + APPNAME + " " + PROCID + " " + MSGID + " ";
+	}
+
+	private static String getHostName(String hostname) {
+		if (hostname.isEmpty()) {
+			try {
+				return InetAddress.getLocalHost().getHostAddress();
+			} catch (UnknownHostException e) {
+				return "-"; // NILVALUE
+			}
+		} else {
+			return hostname;
+		}
+	}
+
+	private static String getValidArgument(String argument) {
+		if (argument.isEmpty()) {
+			return "-"; // NILVALUE
+		} else {
+			return argument;
+		}
+	}
+
+	private static String buildMsg(String msg) {
+		return " " + BOM + msg;
+	}
+
+	private static byte[] buildSyslogMsg(String header, String sd, String msg) {
+
+		ByteArrayOutputStream con = new ByteArrayOutputStream();
+
+		byte[] byteheader = header.getBytes(StandardCharsets.US_ASCII);
+		byte[] byteSD = sd.getBytes();
+		byte[] byteMsg = msg.getBytes(StandardCharsets.UTF_8);
+
+		try {
+			con.write(byteheader);
+			con.write(byteSD);
+			con.write(byteMsg);
+		} catch (Exception e) {
+			System.err.println("Timeout: " + e.getMessage());
+		}
+
+		return con.toByteArray();
+	}
+
+//	private static DatagramPacket createIpRequest(int Port) {
+//
+//		InetAddress broadcast = null;
+//		try {
+//			broadcast = InetAddress.getByName("255.255.255.255");
+//		} catch (Exception e) {
+//			System.err.println(e);
+//		}
+//		return new DatagramPacket(new byte[0], 0, broadcast, PORT);
+//	}
 
 	public static void main(String[] args) {
 
+		if (args.length != 6) {
+			throw new RuntimeException(
+					"Es wurden nicht alle 6 benötigten Kommandozeilenparameter eingegeben. Bitte prüfen Sie Ihre Kommandozeilenparameter und ggf. die Dokumentation dieser Anwendung.");
+		}
+		
 		try (DatagramSocket socket = new DatagramSocket()) {
-			socket.setSoTimeout(TIMEOUT); 
-			InetAddress iaddr = InetAddress.getByName(HOST);
-			DatagramPacket packetOut = new DatagramPacket(new byte[0], 0, iaddr, PORT);
-			socket.send(packetOut);
-			// kein packetIn, da Client keine Antwort bekommen soll
+			socket.setSoTimeout(TIMEOUT);
+
+			System.out.println("Sende autodiscovery message.");
+			InetAddress discoveryIaddr = InetAddress.getByName("255.255.255.255");
+			DatagramPacket discoveryPacketOut = new DatagramPacket(new byte[0], 0, discoveryIaddr, PORT);
+			socket.send(discoveryPacketOut);			
+
+			DatagramPacket discoveryPacketIn = new DatagramPacket(new byte[0], 0);
+			socket.receive(discoveryPacketIn);
+			InetAddress loggingServerIaddr = discoveryPacketIn.getAddress();
 			
+			System.out.println("IP vom Logging-Server gefunden: " + loggingServerIaddr.getHostAddress() + " . Sende Syslog-Nachricht...");
+			
+			String PRI = getPRI(args[0], args[1]);
+			String TIMESTAMP = getTimeStamp();
+			String HOSTNAME = getHostName(args[2]);
+			String APPNAME = getValidArgument(args[3]);
+			String PROCID = getPID();
+			String MSGID = getValidArgument(args[4]);
+
+			// HEADER
+			String header = createHeader(PRI, TIMESTAMP, HOSTNAME, APPNAME, PROCID, MSGID);
+
+			// STRUCTURED DATA
+			String sd = "-";
+
+			// MSG
+			String msg = buildMsg(args[5]);
+
+			// SYSLOG FORMAT MESSAGE
+			byte[] syslogMsg = buildSyslogMsg(header, sd, msg);
+			
+			if (syslogMsg.length > BUFSIZE) {
+				throw new RuntimeException("Die Syslog Nachricht ist mit " + syslogMsg.length + " Bytes zu lang! Maximal "
+						+ BUFSIZE + " Bytes zulässig.");
+			} else {
+				DatagramPacket packetOut = new DatagramPacket(syslogMsg, syslogMsg.length, loggingServerIaddr, PORT);
+				socket.send(packetOut);
+			}
+
+			// AUTO DISCOVERY
+//			socket.send(createIpRequest(PORT));
+//			System.out.println("starting autodiscovery..");
+//
+//			DatagramPacket ipAnswer = new DatagramPacket(new byte[0], 0);
+//			socket.receive(ipAnswer);
+//			System.out.println("  received: " + ipAnswer.getAddress());
+//			InetAddress serverAddr = ipAnswer.getAddress();
+//
+//			// TRANSPORT
+//			DatagramPacket packetOut = new DatagramPacket(syslogMsg, syslogMsg.length, serverAddr, PORT);
+//			if (syslogMsg.length <= BUFSIZE) {
+//				socket.send(packetOut);
+//			}
+
 		} catch (SocketTimeoutException e) {
 			System.err.println("Timeout: " + e.getMessage());
 		} catch (Exception e) {
@@ -33,6 +180,145 @@ public class SyslogClient {
 		}
 	}
 
+	private static int getFacilityCode(String facility) {
+		switch (facility) {
+		case "0":
+			return 0;
+		case "kernel messages":
+			return 0;
+		case "1":
+			return 1;
+		case "user-level messages":
+			return 1;
+		case "2":
+			return 2;
+		case "mail system":
+			return 2;
+		case "3":
+			return 3;
+		case "system daemons":
+			return 3;
+		case "4":
+			return 4;
+		case "security/authorization messages":
+			return 4;
+		case "5":
+			return 5;
+		case "messages generated internally by syslogd":
+			return 5;
+		case "6":
+			return 6;
+		case "line printer subsystem":
+			return 6;
+		case "7":
+			return 7;
+		case "network news subsystem":
+			return 7;
+		case "8":
+			return 8;
+		case "UUCP subsystem":
+			return 8;
+		case "9":
+			return 9;
+		case "clock daemon":
+			return 9;
+		case "10":
+			return 10;
+		case "11":
+			return 11;
+		case "FTP daemon":
+			return 11;
+		case "12":
+			return 12;
+		case "NTP subsystem":
+			return 12;
+		case "13":
+			return 13;
+		case "log audit":
+			return 13;
+		case "14":
+			return 14;
+		case "log alert":
+			return 14;
+		case "15":
+			return 15;
+		case "clock daemon (note 2)":
+			return 15;
+		case "16":
+			return 16;
+		case "local0":
+			return 16;
+		case "17":
+			return 17;
+		case "local1":
+			return 17;
+		case "18":
+			return 18;
+		case "local2":
+			return 18;
+		case "19":
+			return 19;
+		case "local3":
+			return 19;
+		case "20":
+			return 20;
+		case "local4":
+			return 20;
+		case "21":
+			return 21;
+		case "local5":
+			return 21;
+		case "22":
+			return 22;
+		case "local6":
+			return 22;
+		case "23":
+			return 23;
+		case "local7":
+			return 23;
+		default:
+			throw new RuntimeException(facility + " ist keine valide Eingabe für die facility!");
+		}
+
+	}
+
+	private static int getSeverityCode(String severity) {
+		switch (severity) {
+		case "0":
+			return 0;
+		case "Emergency":
+			return 0;
+		case "1":
+			return 1;
+		case "Alert":
+			return 1;
+		case "2":
+			return 2;
+		case "Critical":
+			return 2;
+		case "3":
+			return 3;
+		case "Error":
+			return 3;
+		case "4":
+			return 4;
+		case "Warning":
+			return 4;
+		case "5":
+			return 5;
+		case "Notice":
+			return 5;
+		case "6":
+			return 6;
+		case "Informational":
+			return 6;
+		case "7":
+			return 7;
+		case "Debug":
+			return 7;
+		default:
+			throw new RuntimeException(severity + " ist keine valide Eingabe für severity!");
+		}
+	}
+
 }
-
-
--- src/LoggingService/SyslogServer.java
+++ src/LoggingService/SyslogServer.java
@@ -6,34 +6,34 @@ import java.net.DatagramSocket;
 import java.util.Date;
 
 public class SyslogServer {
+	private static final int PORT = 4711;
+	private static final int BUFSIZE = 2048;
 
-
-	private static final int PORT = 4712; 
-
-	private static final int BUFSIZE = 512;
-
-
-	public static void main(String[] args) {
-		
+	public static void main(final String[] args) {
 		try (DatagramSocket socket = new DatagramSocket(PORT)) {
-			
 			DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
-			// kein packetOut notwendig, da keine Antwort erwartet wird
+			DatagramPacket discoveryPacketOut = new DatagramPacket(new byte[0], 0);
 
 			System.out.println("Server gestartet ...");
 
 			while (true) {
+
 				socket.receive(packetIn);
-				System.out.println(
-						"Received from: " + packetIn.getAddress().getHostAddress() + ":" + packetIn.getPort());
-				String jetzt = (new Date()).toString();
+
+				if (packetIn.getLength() == 0) { // discovery packet
+					discoveryPacketOut.setSocketAddress(packetIn.getSocketAddress());
+					socket.send(discoveryPacketOut);
+				} else { // logging packet
+					String log = new String(packetIn.getData(), 0, packetIn.getLength());
+
+					System.out.println("Neue Syslog-Nachricht von " + packetIn.getAddress().getHostAddress()
+							+ " (Hostname: " + packetIn.getAddress().getHostName() + ") erhalten: ");
+					System.out.println(log);
+				}
 
 			}
-		} catch (final IOException e) {
+		} catch (IOException e) {
 			System.err.println(e);
 		}
 	}
-
 }
-
-
