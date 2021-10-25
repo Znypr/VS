4f1911bd3fb4848a06195fe7e275f04f1416a6f8
Mike <mike.eirich@gmx.de>
2021-10-25 16:37:04 +0200

    speedtest-commit

--- src/SpeedTest/SpeedtestClient.java
+++ src/SpeedTest/SpeedtestClient.java
@@ -4,35 +4,110 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.text.DecimalFormat;
 
 public class SpeedtestClient {
+
+
+	private static final String HOST = "localhost";
+
+	private static final int PORT = 4711;
+
+	private static final int BUFSIZE = 65000;
+
+	private static final int TIMEOUT = 2000;
+	
+	private static final int RUNS = 5;
+	
+	private static final DecimalFormat f = new DecimalFormat("##.00");
 	
 	
-	  private static final String HOST = "localhost";
+	private static float getAverage(long[] array) {
+		 long avg = 0;
+		 
+		 for(int i=0; i<array.length; i++) {
+			 avg += array[i];
+		 }
+		 
+		 return avg/array.length;
+	}
 
-	  private static final int PORT = 4712;
-
-	  private static final int BUFSIZE = 512;
-
-	  private static final int TIMEOUT = 2000;
-
-	  
 	public static void main(String[] args) {
 		
-	    try (DatagramSocket socket = new DatagramSocket()) {
-	        socket.setSoTimeout(TIMEOUT); 
-	        InetAddress iaddr = InetAddress.getByName(HOST);
-	        DatagramPacket packetOut = new DatagramPacket(new byte[0], 0, iaddr, PORT);
-	        socket.send(packetOut);
-	        DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
-	        socket.receive(packetIn);
-	        String received = new String(packetIn.getData(), 0, packetIn.getLength());
-	        System.out.println("Received: " + received);
-	      } catch (SocketTimeoutException e) {
-	        System.err.println("Timeout: " + e.getMessage());
-	      } catch (Exception e) {
-	        System.err.println(e);
-	      }
- 
+		long startTime = 0;
+		long stopTime = 0;
+
+		try (DatagramSocket socket = new DatagramSocket()) {
+
+			socket.setSoTimeout(TIMEOUT); 
+			InetAddress iaddr = InetAddress.getByName(HOST);	     
+			
+			
+		// LATENCY 
+		
+			long[] latency = new long[RUNS];
+			
+			for(int i=0; i<RUNS; i++) {
+				
+				DatagramPacket latencyOut = new DatagramPacket(new byte[0], 0, iaddr, PORT);
+				startTime = System.nanoTime();
+				socket.send(latencyOut);
+				
+				DatagramPacket latencyIn = new DatagramPacket(new byte[0], 0);
+				socket.receive(latencyIn);
+				stopTime = System.nanoTime();
+				
+				latency[i] = stopTime-startTime;
+			}
+			float latency_ms = (float) (getAverage(latency)/1e6);
+			System.out.println("Latency:        " + f.format(latency_ms) + "ms");
+		
+	
+		// UPLOAD 
+			
+			long[] uploadTime = new long[RUNS];
+					
+			for(int i=0; i<RUNS; i++) {
+					
+				DatagramPacket uploadOut = new DatagramPacket(new byte[BUFSIZE], BUFSIZE, iaddr, PORT);
+				startTime = System.nanoTime();
+				socket.send(uploadOut);
+				
+				DatagramPacket uploadIn = new DatagramPacket(new byte[0], 0);
+				socket.receive(uploadIn);
+				stopTime = System.nanoTime();
+						
+				uploadTime[i] = stopTime-startTime;
+			}
+			float uploadSpeed = ((float) 1e9 / getAverage(uploadTime) * BUFSIZE) / 1000 / 1000;
+			System.out.println("Upload Speed:   " + f.format(uploadSpeed) + "MBps");
+		
+		
+		// DOWNLOAD 
+		
+			long[] downloadTime = new long[RUNS];
+					
+			for(int i=0; i<RUNS; i++) {
+					
+				DatagramPacket downloadOut = new DatagramPacket(new byte[0], 0, iaddr, PORT);
+				startTime = System.nanoTime();
+				socket.send(downloadOut);
+				
+				DatagramPacket downloadIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
+				socket.receive(downloadIn);
+				stopTime = System.nanoTime();
+				
+				downloadTime[i] = stopTime-startTime;
+			}
+			float downloadSpeed = ((float) 1e9 / getAverage(downloadTime) * BUFSIZE) / 1000 / 1000;
+			System.out.println("Download Speed: " + f.format(downloadSpeed) + "MBps");
+				
+		
+
+		} catch (SocketTimeoutException e) {
+			System.err.println("Timeout: " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
 }
--- src/SpeedTest/SpeedtestServer.java
+++ src/SpeedTest/SpeedtestServer.java
@@ -3,33 +3,56 @@ package SpeedTest;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.Date;
 
 public class SpeedtestServer {
 
 
-	private static final int PORT = 4712;
+	private static final int PORT = 4711;
 
-	private static final int BUFSIZE = 512;
+	private static final int BUFSIZE = 65000;
 
 
 	public static void main(String[] args) {
 
 		try (DatagramSocket socket = new DatagramSocket(PORT)) {
-			DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
-			DatagramPacket packetOut = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
 
-			System.out.println("Server gestartet ...");
+			System.out.println("starting server..\n");		
 
 			while (true) {
-				socket.receive(packetIn);
-				System.out.println(
-						"Received from: " + packetIn.getAddress().getHostAddress() + ":" + packetIn.getPort());
-				String jetzt = (new Date()).toString();
-				packetOut.setData(jetzt.getBytes());
-				packetOut.setLength(jetzt.length());
-				packetOut.setSocketAddress(packetIn.getSocketAddress());
-				socket.send(packetOut);
+				
+			// LATENCY 
+			
+				DatagramPacket latencyIn = new DatagramPacket(new byte[0], 0);
+				socket.receive(latencyIn);
+				
+				DatagramPacket latencyOut = new DatagramPacket(new byte[0], 0);
+				latencyOut.setSocketAddress(latencyIn.getSocketAddress());
+				socket.send(latencyOut);
+			
+			
+				
+			// UPLOAD 
+			
+				DatagramPacket uploadIn = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
+				socket.receive(uploadIn);
+							
+				DatagramPacket uploadOut = new DatagramPacket(new byte[0], 0);
+				uploadOut.setSocketAddress(uploadIn.getSocketAddress());
+				socket.send(uploadOut);
+			
+			
+			
+			// DOWNLOAD 
+			
+				DatagramPacket downloadIn = new DatagramPacket(new byte[0], 0);
+				socket.receive(downloadIn);
+							
+				DatagramPacket downloadOut = new DatagramPacket(new byte[BUFSIZE], BUFSIZE);
+				downloadOut.setSocketAddress(downloadIn.getSocketAddress());
+				socket.send(downloadOut);
+				
+
+
 			}
 		} catch (final IOException e) {
 			System.err.println(e);
